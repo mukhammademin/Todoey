ce213b71866760ea0bec3a0313571d3783d21e73
Mukhammademin <mukhammademin19@icloud.com>
2023-03-15 14:32:34 -0700

    upgrade wedMarch15

--- Todoey/Controller/ViewController.swift
+++ Todoey/Controller/ViewController.swift
@@ -6,17 +6,23 @@
 //
 
 import UIKit
+import CoreData
+
 
 class ViewController: UITableViewController {
     
     var itemArray = [Item]()
     
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-
+    
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(dataFilePath!)
         
+        loadData()
+        //print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist"))
+       
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -29,21 +35,26 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = item.title
         
         //Ternary oparator
-        //value = condition ? valueTrue : valueFalse
-        
+        //value = condition ? valueTrue : valueFalsen
         cell.accessoryType = item.isChecked == true ? .checkmark : .none
         
-//        if item.isChecked == true { cell.accessoryType = .checkmark
-//        } else { cell.accessoryType = .none }
+        //        if item.isChecked == true { cell.accessoryType = .checkmark
+        //        } else { cell.accessoryType = .none }
         return cell
     }
-
+    
     //MARK: - TableView Delegate Methods
     
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
         itemArray[indexPath.row].isChecked = !itemArray[indexPath.row].isChecked
         saveItems()
+        
+        
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
@@ -58,37 +69,69 @@ class ViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let action =  UIAlertAction(title: "Add Item", style: .default) { (action) in
+      
             
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
             
             self.itemArray.append(newItem)
-            
+            newItem.isChecked = false
             self.saveItems()
             
-            }
-        
+        }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
-            
             textField = alertTextField
-            
         }
-    
         alert.addAction(action)
-        
         present(alert, animated: true, completion: nil)
     }
+    
+    //MARK: - Model Manipulation Methods
+    
     func saveItems() {
-        let encoder = PropertyListEncoder()
-        
+        //let encoder = PropertyListEncoder()
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
             print(error)
         }
         self.tableView.reloadData()
     }
+    
+    func loadData(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print(error)
+        }
+        tableView.reloadData()
+    }
 }
+//MARK: - UISearchBarDelegate
 
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadData(with: request)
+        
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadData()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
+        
+    }
+}
